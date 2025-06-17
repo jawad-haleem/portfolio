d174b407485878be10db519842e8b8076d045ff7
Jawad <muhammadjawadhaleem@preesoft.com>
2025-06-17 19:55:21 +0500

    Revert "updated AI engineering post experience"
    This reverts commit fbdf1dd7d1cf8480895e49228f31a57ef5f02703.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,15 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0a192f",
-        secondary: "#64ffda",
-        tertiary: "#112240",
-        "black-100": "#1a1a2e",
-        "black-200": "#16213e",
-        "white-100": "#e6f1ff",
+        primary: "#050816",
+        secondary: "#B7BEFF",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px rgba(100, 255, 218, 0.15)",
+        card: "0px 35px 120px -15px #211e35",
       },
       screens: {
         xs: "450px",
